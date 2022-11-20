4dcb60b9fb60b8486b91ecdb11b020756583303d
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-20 19:23:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,63 @@
 import React from "react";
-import Feedback from "./Feedback";
-import styled from 'styled-components'
+import Statistics from "./Statistics";
+import { FeedbackOptions } from './FeedbackOptions'
+import { Sections } from './Sections'
+import { Notification } from "./Notification";
 
-export const HeadTitle = styled.h1`
-  margin-right: 15px;
-  margin-left: 15px;
-  font-size: 64px;
-  font-family: monospace;
-  text-align: center;
-`
-
-export const App = () =>
+export class App extends React.Component
 {
-  return (
-    <>
-      <HeadTitle>Cafe Expresso</HeadTitle>
-      <Feedback />
-    </>
-  );
-};
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    }
+
+    handleChange = (event) =>
+    {
+      const { name } = event.target
+      this.setState((prevState) => ({ [name]: prevState[name] + 1 }))   
+    }
+
+    countTotalFeedback = () =>
+    {
+        const { good, neutral, bad } = this.state;
+        const total = good + neutral + bad
+        return total;
+    }
+
+    countPositiveFeedbackPercentage = () =>
+    {
+        return ((this.state.good / this.countTotalFeedback()) * 100).toFixed(0)
+    }
+
+    render()
+    {
+      const { good, neutral, bad } = this.state;
+      const total = this.countTotalFeedback();
+      const positivePercentage = this.countPositiveFeedbackPercentage();
+      return (
+          <>
+            <Sections title="Cafe Expresso"></Sections>
+            <Sections title="Please leave feedback"> 
+              <FeedbackOptions
+                options={Object.keys(this.state)}
+                onLeaveFeedback={this.handleChange}
+              >
+                </FeedbackOptions>
+            </Sections>
+            <Sections title="Statistics"> 
+              {this.countTotalFeedback() === 0
+                ?
+                <Notification message="There is no feedback"></Notification>
+                : <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage} />
+          }
+          </Sections>
+          </>
+        )
+    }
+}
