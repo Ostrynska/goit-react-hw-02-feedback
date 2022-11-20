ecf61596be414a58e6d6518fb8c4bb17c83b8f5f
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-20 19:23:44 +0200

    Create FeedbackOptions.jsx

---
+++ src/components/FeedbackOptions.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) =>
+{
+    return (
+        <ul >
+            {options.map(option => (
+                <li key={option}>
+                    <button
+                        type="button"
+                        name={option}
+                        onClick={onLeaveFeedback}>{option}
+                    </button>
+                </li>))
+            }
+        </ul>   
+    )
+};
