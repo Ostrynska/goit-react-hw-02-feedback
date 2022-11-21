e682ff99e0d76860b05ed01b42e0cb13cf29a4b6
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-21 14:17:22 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import PropTypes from 'prop-types';
+import { StatisticList, StatisticItem, StatisticValue } from "./Statistics.styled";
 
-export const Statistics = ({ good, neutral, bad, total, positivePercentage }) =>
-    {
-        return (
-            <>
-                <h2>Statistics</h2>
-                    <p>Good: { good }</p>
-                    <p>Neutural: { neutral }</p>
-                    <p>Bad: { bad }</p>
-                    <p>Total: {total}</p>
-                    <p>Positive feedback: {positivePercentage}%</p>
-            </>
-        )
-    }
 
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) =>
+{
+      return(
+            <StatisticList>
+                  <StatisticItem>Good:
+                        <StatisticValue>{good}</StatisticValue>
+                  </StatisticItem>
+                  <StatisticItem>Neutral:
+                        <StatisticValue>{neutral}</StatisticValue>
+                  </StatisticItem>
+                  <StatisticItem>Bad:
+                        <StatisticValue>{bad}</StatisticValue>
+                  </StatisticItem>
+                  <StatisticItem>Total:
+                        <StatisticValue>{total}</StatisticValue>
+                  </StatisticItem>
+                  <StatisticItem>Positive feedback:
+                        <StatisticValue>{positivePercentage}%</StatisticValue>
+                  </StatisticItem>
+            </StatisticList> 
+      )
+}
+
+Statistics.propTypes = {
+      good: PropTypes.number.isRequired,
+      neutral: PropTypes.number.isRequired,
+      bad: PropTypes.number.isRequired,
+      total: PropTypes.number.isRequired,
+      positivePercentage: PropTypes.number.isRequired,
+}
 
 export default Statistics
